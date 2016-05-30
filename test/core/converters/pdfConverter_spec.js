ec5794b5906a50bbe49e1f4a74bb246db68ae5c3
Robin de Mourat <robin.demourat@gmail.com>
2016-05-30 11:10:15 +0200

    prepare test data for images ctxt

--- test/core/converters/pdfConverter_spec.js
+++ test/core/converters/pdfConverter_spec.js
@@ -1,22 +1,32 @@
 import {expect} from 'chai';
+import {waterfall} from 'async';
 const cobaye = require('./../../_sample_content/parsing_output.json')
 import {exportSection} from './../../../src/core/converters/pdfConverter';
 
+import resolveDataDependencies from './../../../src/core/resolvers/resolveDataDependencies';
+
 import {sample_assets_path} from "./../../test_settings.json";
-const assets_path = __dirname + '/../../' + sample_assets_path;
 import * as assetsController from './../../../src/core/controllers/assetsController';
+const base_assets_path = __dirname + '/../../' + sample_assets_path;
 
 const assetsParams = {
-  mode: 'filesystem',
-  basePath: assets_path
+  connector: 'filesystem',
+  basePath: base_assets_path
 };
+
 
 describe('pdfConverter:exportsection', function(){
   it('should convert to pdf', function(done){
-    const sections = cobaye.sections;
-    exportSection({section: sections[0], sectionList: sections}, function(err, res) {
-      console.log('done');
-      done();
+    const inputSections = cobaye.sections;
+    waterfall([
+      function(callback) {
+        resolveDataDependencies(inputSections, assetsController, assetsParams, callback);
+      }
+    ], function(err, sections){
+      exportSection({section: sections[0], sectionList: sections}, function(err, res) {
+        console.log('done');
+        done();
+      });
     });
   });
 })
