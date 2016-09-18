ad587b9340a3b69d9f3a8f4b7a0acf76ec13654f
Robin de Mourat <robin.demourat@gmail.com>
2016-09-18 15:58:49 +0200

    clear tests before rebuilding them

--- test/core/controllers/contentsController_spec.js
+++ test/core/controllers/contentsController_spec.js
@@ -14,16 +14,16 @@ const base_output = __dirname + '/../../' + sample_output_path;
 const inputParams = {
   basePath: base_path,
   connector: 'filesystem'
-}
+};
 
 const outputParams = {
   basePath: base_output,
   connector: 'filesystem'
-}
-
+};
+/*
 let params;
 
-describe('contentController:updateFromSource', function(){
+describe('contentController:updateFromSource', () =>{
   it('should update unfaulted data from source without processing errors', function(done){
     lib.updateFromSource(inputParams, models, defaultParameters, function(err, results){
       expect(err).to.be.null;
@@ -33,13 +33,13 @@ describe('contentController:updateFromSource', function(){
   });
 });
 
-describe('contentController:updateToSource', function(){
+describe('contentController:updateToSource', () => {
   params = outputParams;
   it('should update unfaulted data from one source to another without breaking', function(done){
     let inputFsTree, document;
     waterfall([
       //delete test folder
-      function(cb){
+      (cb) => {
         exists(base_output, function(isThere){
           if (isThere) {
             connector.deleteFromPath({params}, cb);
@@ -49,36 +49,37 @@ describe('contentController:updateToSource', function(){
         });
       },
       //recreate test folder
-      function(cb){
+      (cb) => {
         connector.createFromPath({params, type: 'directory', overwrite: true}, function(err, response){
           cb();
         });
       },
       //make a fs representation of the test folder
-      function(cb) {
+      (cb) => {
         connector.readFromPath({params, depth: true, parseFiles: true}, function(err, results) {
           inputFsTree = results;
           cb();
         });
       },
       //fill the data to update
-      function(cb) {
+      (cb) => {
         lib.updateFromSource(inputParams, models, defaultParameters, function(err, results){
           document = results.document;
           cb();
         });
       },
       // update the data (fs tree changes)
-      function(cb){
+      (cb) => {
         lib.updateToSource(outputParams, document, models, inputFsTree, function(err, newFsTree){
           cb(err, newFsTree);
         });
       },
     ],
-    function(err, results) {
+    (err, results) => {
       expect(err).to.be.null;
       expect(results).to.be.defined;
       done();
     });
   });
 });
+*/
