2047e7cf2fd87f613fd984fe7e789835f27fc524
Robin de Mourat <robin.demourat@gmail.com>
2016-12-16 17:10:51 +0100

    incude contextualizations id in lexicon rendering

--- src/core/getters.js
+++ src/core/getters.js
@@ -112,6 +112,7 @@ export const getGlossary = (document, preRenderContexts = true) => {
         }
         return localResults.concat({
           mentionId: '#peritext-content-entity-inline-' + sectionCitekey + '-' + contextualization.id,
+          contextualizationId: contextualization.id,
           entity: document.resources[contextualization.resources[0]].id,
           alias: document.contextualizers[contextualization.contextualizer].alias,
           targetBlockPath,
